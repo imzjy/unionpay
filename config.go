b3cf35271e0bef593260cdec365630b20ed269b0
zjy <imjatsz@gmail.com>
2015-06-03 09:14:57 +0800

    add openssl command to convert pkcs12 to key and cert

--- config.go
+++ config.go
@@ -1,8 +1,8 @@
 package gounionpay
 
 type UnionpayConfig struct {
-	SignKeyPath    string //加密密钥路径(convert from *.pfx)
-	SignCertPath   string //加密证书路径(convert from *.pfx)
+	SignKeyPath    string //加密密钥路径(openssl pkcs12 -in PM_700000000000001_acp.pfx -clcerts -nokeys -out key.cert)
+	SignCertPath   string //加密证书路径(openssl pkcs12 -in PM_700000000000001_acp.pfx -nocerts -nodes -out key.pem)
 	VerifyCertPath string //验证证书路径
 
 	CallbackUrl string //回调地址
